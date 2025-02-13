0b6361ef3ce8802816edb3985cdcf52612f9a462
kindness-ok <kindnessokpugie1@gmail.com>
2025-02-13 23:08:32 +0100

    Day 11 unsolved

--- day-11.js
+++ day-11.js
@@ -1,2 +1,21 @@
 // Day 11
+const rememberIt = (fn) => {
+  return function (...args) {
+    let memory = [];
+    console.log(...args, 'args', args)
+    memory.push(...args);
+    if (memory.includes(args)) return undefined;
+    const result = fn(...args);
+    console.log(memory, 'MEMMM', result);
+  }
+}
 
+let callCount = 0;
+const memoizedFn = rememberIt(function (a, b) {
+  callCount += 1;
+  return a + b;
+})
+memoizedFn(2, 3) // 5
+memoizedFn(4, 5) // 5
+memoizedFn(4, 5) // 5
+console.log(callCount) // 1
