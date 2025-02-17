655c911df4e6d73f4456c0a6ec2449d38d1f6de3
kindness-ok <kindnessokpugie1@gmail.com>
2025-02-17 22:02:40 +0100

    day 10 solution

--- day-11.js
+++ day-11.js
@@ -8,14 +8,14 @@ const rememberIt = (fn) => {
     const result = fn(...args);
     console.log(memory, 'MEMMM', result);
   }
-}
+};
 
 let callCount = 0;
 const memoizedFn = rememberIt(function (a, b) {
   callCount += 1;
   return a + b;
-})
+});
 memoizedFn(2, 3) // 5
-memoizedFn(4, 5) // 5
+memoizedFn(4, 5) // 9
 memoizedFn(4, 5) // 5
 console.log(callCount) // 1
